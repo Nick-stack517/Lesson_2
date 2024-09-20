8b7f76510c99ead8e086cc648aa38382668f2714
Nick-stack517 <gsatello@gmail.com>
2024-09-21 00:05:32 +0700

    Add files via upload

---
+++ Lesson_2.py
@@ -0,0 +1,5 @@
+homework = 12
+time_spent = 1.5
+name_of_the_course = 'Python'
+execution_time = time_spent / homework
+print('Курс:',name_of_the_course,', всего задач:',homework,', затрачено часов:',time_spent,', среднее время выполнения:',execution_time)
